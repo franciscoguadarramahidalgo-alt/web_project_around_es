fb2f3a4029b5114203761f2dda7023d3b2f60e0d
FrankGu <francisco.guadarrama.hidalgo@gmail.com>
2026-09-18 04:43:11 -0600

    correct reusable functions words modal for popup

--- scripts/index.js
+++ scripts/index.js
@@ -28,14 +28,15 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
 const editProfileButton = document.querySelector(`.profile__edit-button`);
 const closeProfileButton = document.querySelector(`.popup__close`);
 const editProfileModal = document.querySelector(`#edit-popup`);
 function openModal(modalElement) {
-  modalElement.classList.add(`modal_is-opened`);
+  modalElement.classList.add(`popup_is-opened`);
 }
 function closeModal(modalElement) {
-  modalElement.classList.remove(`modal_is-opened`);
+  modalElement.classList.remove(`popup_is-opened`);
 }
 editProfileButton.addEventListener(`click`, function () {
   openModal(editProfileModal);
